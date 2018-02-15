51c813bc471232f89a72b26abeb6086697be2f98
jprothkegel <juan.rothkegel.12@sansano.usm.cl>
2018-02-15 15:02:17 -0300

    Modificado: Card agregadas de comidas

--- src/components/Menu/Menus.vue
+++ src/components/Menu/Menus.vue
@@ -1,3 +1,63 @@
 <template>
-  <p>Lista de menus</p>
+  <v-container grid-list-md>
+    <v-layout>
+      <v-flex xs6 sm6>
+        <v-card>
+          <v-card-media
+            class="white--text"
+            height="200px"
+            src="/static/images/almuerzo.jpg"
+          >
+            <v-container fill-height fluid>
+              <v-layout fill-height>
+                <v-flex xs12 align-end flexbox>
+                  <span class="headline">Top 10 Australian beaches</span>
+                </v-flex>
+              </v-layout>
+            </v-container>
+          </v-card-media>
+          <v-card-title>
+            <div>
+              <span class="grey--text">Number 10</span><br>
+              <span>Whitehaven Beach</span><br>
+              <span>Whitsunday Island, Whitsunday Islands</span>
+            </div>
+          </v-card-title>
+          <v-card-actions>
+            <v-btn flat color="orange">Share</v-btn>
+            <v-btn flat color="orange">Explore</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+
+      <v-flex xs6 sm6>
+        <v-card>
+          <v-card-media
+            class="white--text"
+            height="200px"
+            src="/static/images/almuerzo.jpg"
+          >
+            <v-container fill-height fluid>
+              <v-layout fill-height>
+                <v-flex xs12 align-end flexbox>
+                  <span class="headline">Top 10 Australian beaches</span>
+                </v-flex>
+              </v-layout>
+            </v-container>
+          </v-card-media>
+          <v-card-title>
+            <div>
+              <span class="grey--text">Number 10</span><br>
+              <span>Whitehaven Beach</span><br>
+              <span>Whitsunday Island, Whitsunday Islands</span>
+            </div>
+          </v-card-title>
+          <v-card-actions>
+            <v-btn flat color="orange">Share</v-btn>
+            <v-btn flat color="orange">Explore</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
