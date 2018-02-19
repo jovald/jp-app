cb0d83f8921b053a03bbd75decbc0224ea8e6621
jprothkegel <juan.rothkegel.12@sansano.usm.cl>
2018-02-19 09:55:11 -0300

    datepicker arreglado

--- src/components/Menu/Menu.vue
+++ src/components/Menu/Menu.vue
@@ -1,7 +1,7 @@
 <template>
-
     <v-flex xs10 offset-xs1 >
       <v-card>
+      <v-flex xs10 offset-xs1 >
       <v-form v-model="valid" ref="form" lazy-validation>
         <v-text-field
           label="Almuerzo"
@@ -24,7 +24,6 @@
           :first-day-of-week="1"
           locale="es-cl"
           v-model="picker"
-          :allowed-dates="allowedDates"
           v-bind:min="fechaMin"
           v-bind:max="fechaMax"
         ></v-date-picker>
@@ -40,6 +39,7 @@
         </v-card-actions>
 
       </v-form>
+      </v-flex>
       </v-card>
     </v-flex>
 
@@ -49,10 +49,14 @@
 <script>
   import axios from 'axios'
 
+
   export default {
+    created: function(){
+      this.getActualWeek();
+    },
     data: () => ({
-      fechaMin: '2016-06-15',
-      fechaMax: '2017-03-20',
+      fechaMin: '',
+      fechaMax: '',
       valid: true,
       almuerzo: '',
       almuerzoRules: [
@@ -66,6 +70,18 @@
     }),
 
     methods: {
+      getActualWeek: function(){
+        var curr = new Date; // get current date
+        var first = curr.getDate() - curr.getDay() +1; // First day is the day of the month - the day of the week
+        var last = first + 5; // last day is the first day + 6
+
+        var firstday = new Date(curr.setDate(first)).toISOString();
+        var lastday = new Date(curr.setDate(last)).toISOString();
+        firstday = firstday.split('T').shift();
+        lastday = lastday.split('T').shift();
+        this.fechaMin = firstday;
+        this.fechaMax = lastday;
+      },
       submit () {
         if (this.$refs.form.validate()) {
           // Native form submission is not yet supported
