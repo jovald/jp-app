a82f82c75365d4f5f6e1c1725e3e30b603718fb2
jprothkegel <juan.rothkegel.12@sansano.usm.cl>
2018-02-20 19:24:35 -0300

    tabla corregida

--- src/components/Menu/Menu.vue
+++ src/components/Menu/Menu.vue
@@ -34,9 +34,10 @@
                 color="red darken-1"
                 :first-day-of-week="1"
                 locale="es-cl"
-                v-model="picker"
+                v-model="fecha"
                 v-bind:min="fechaMin"
                 v-bind:max="fechaMax"
+                required
               ></v-date-picker>
 
               <v-card-actions>
@@ -80,12 +81,19 @@
           </v-tooltip>
         </template>
         <template slot="items" slot-scope="props">
-          <td>{{ props.item.name }}</td>
-          <td class="text-xs-right">{{ props.item.calories }}</td>
-          <td class="text-xs-right">{{ props.item.fat }}</td>
-          <td class="text-xs-right">{{ props.item.carbs }}</td>
-          <td class="text-xs-right">{{ props.item.protein }}</td>
-          <td class="text-xs-right">{{ props.item.iron }}</td>
+          <td>{{ props.item.almuerzo }}</td>
+          <td>{{ props.item.cena }}</td>
+          <td>{{ props.item.fecha }}</td>
+          <td>
+            <v-btn
+              @click="deleting(props.item)"
+              :disabled="!valid"
+              color="primary"
+            >
+            Eliminar
+            </v-btn>
+
+          </td>
         </template>
       </v-data-table>
 
@@ -104,7 +112,7 @@
       this.getActualWeek()
     },
     data: () => ({
-      picker: '',
+      fecha: '',
       fechaMin: '',
       fechaMax: '',
       valid: true,
@@ -116,111 +124,18 @@
       cenaRules: [
         v => !!v || 'Debes ingresar una cena'
       ],
+
       headers: [
           {
-            text: 'Dessert (100g serving)',
-            align: 'left',
-            sortable: false,
-            value: 'name'
+            text: 'Almuerzo',
+            value: 'almuerzo'
           },
-          { text: 'Calories', value: 'calories' },
-          { text: 'Fat (g)', value: 'fat' },
-          { text: 'Carbs (g)', value: 'carbs' },
-          { text: 'Protein (g)', value: 'protein' },
-          { text: 'Iron (%)', value: 'iron' }
+          { text: 'Cena',
+            value: 'Cena' },
+          { text: 'Fecha', value: 'fecha' },
+          { text: 'Editar/Eliminar', sortable: false, value: 'accion' }
         ],
-        items: [
-          {
-            value: false,
-            name: 'Frozen Yogurt',
-            calories: 159,
-            fat: 6.0,
-            carbs: 24,
-            protein: 4.0,
-            iron: '1%'
-          },
-          {
-            value: false,
-            name: 'Ice cream sandwich',
-            calories: 237,
-            fat: 9.0,
-            carbs: 37,
-            protein: 4.3,
-            iron: '1%'
-          },
-          {
-            value: false,
-            name: 'Eclair',
-            calories: 262,
-            fat: 16.0,
-            carbs: 23,
-            protein: 6.0,
-            iron: '7%'
-          },
-          {
-            value: false,
-            name: 'Cupcake',
-            calories: 305,
-            fat: 3.7,
-            carbs: 67,
-            protein: 4.3,
-            iron: '8%'
-          },
-          {
-            value: false,
-            name: 'Gingerbread',
-            calories: 356,
-            fat: 16.0,
-            carbs: 49,
-            protein: 3.9,
-            iron: '16%'
-          },
-          {
-            value: false,
-            name: 'Jelly bean',
-            calories: 375,
-            fat: 0.0,
-            carbs: 94,
-            protein: 0.0,
-            iron: '0%'
-          },
-          {
-            value: false,
-            name: 'Lollipop',
-            calories: 392,
-            fat: 0.2,
-            carbs: 98,
-            protein: 0,
-            iron: '2%'
-          },
-          {
-            value: false,
-            name: 'Honeycomb',
-            calories: 408,
-            fat: 3.2,
-            carbs: 87,
-            protein: 6.5,
-            iron: '45%'
-          },
-          {
-            value: false,
-            name: 'Donut',
-            calories: 452,
-            fat: 25.0,
-            carbs: 51,
-            protein: 4.9,
-            iron: '22%'
-          },
-          {
-            value: false,
-            name: 'KitKat',
-            calories: 518,
-            fat: 26.0,
-            carbs: 65,
-            protein: 7,
-            iron: '6%'
-          }
-        ]
+        items: []
 
     }),
 
@@ -239,12 +154,19 @@
       },
       submit () {
         if (this.$refs.form.validate()) {
+          console.log(this.almuerzo);
+          var item = {almuerzo: this.almuerzo, cena: this.cena, fecha: this.fecha}
+          console.log(item);
+          this.items.push(item)
           // Native form submission is not yet supported
 
         }
       },
       clear () {
         this.$refs.form.reset()
+      },
+      deleting (item) {
+        this.items.splice(this.items.indexOf(item), 1)
       }
 
     }
